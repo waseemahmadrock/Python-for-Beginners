eb6a77fb506a65dfb159a75a2b8d56aa5dd694d6
Waseem Ahmad <waseemahmadrock@gmail.com>
2017-08-26 18:25:02 +0500

    Python-for-beginners

---
+++ functions.py
@@ -0,0 +1,16 @@
+def add():
+    return 2 + 3
+
+def multiply():
+    return 2 * 3
+
+def cube():
+    return 2 ** 3
+
+def square_area(side_length=4):
+    area = side_length * 2
+    return area
+
+def rectangle_perimeter(width, height):
+    perimeter = width * 2 + height * 2
+    return perimeter
